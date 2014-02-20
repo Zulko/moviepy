c02d1520c7e0ddbbf0c0c893b2a0f3a9cdcd47a1
Zulko <valentin.zulkower@gmail.com>
2014-02-20 21:31:03 +0100

    Added theme kr for docs. Added embryo of test suite (really an embryo)

---
+++ examples/clips_array.py
@@ -0,0 +1,3 @@
+from moviepy.editor import *
+
+
---
+++ tests/tests.py
@@ -0,0 +1,12 @@
+"""
+Tests meant to be run with pytest
+"""
+
+import pytest
+
+from moviepy.editor import *
+
+
+@pytest.fixture
+def example_video1():
+	
