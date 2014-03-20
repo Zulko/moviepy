fixed bug in rotation, buggy typos in ImageClip fl_image and fl_time

moviepy/video/fx/rotation.py
@@ -1,17 +1,24 @@
-
-
+import numpy as np
 
 def rotation(clip, angle, apply_to_mask=True, unit='deg'):
+    
     if unit == 'rad':
         angle = 360*angle/(2*3.14159)
-    if angle ==90:
-        return clip.fl_image(lambda im : im.T[::-1],
-                              apply_to='mask' if apply_to_mask else [])
-    elif angle == -90:
-        return clip.fl_image(lambda im : im.T,
-                              apply_to='mask' if apply_to_mask else [])
-    elif angle == 180:
-        return clip.fl_image(lambda im : im[::-1,::-1],
-                              apply_to='mask' if apply_to_mask else [])
+    
+    transpo = [1,0] if clip.ismask else [1,0,2]
+    angle_transfo = {
+        90 : lambda im : np.transpose(im, axes=transpo)[::-1],
+        -90: lambda im : np.transpose(im, axes=transpo)[:,::-1],
+        180: lambda im : im[::-1,::-1]}
+    
+    if angle in angle_transfo:
+        newclip =  clip.fl_image( angle_transfo[angle])
     else:
-        raise 'Angle not supported, only 90, -90, 180'
+        raise ValueError('Angle not supported, only 90, -90, 180')
+    
+    if apply_to_mask and (newclip.mask is not None):
+        newclip.mask = newclip.mask.fx( rotation, angle,
+                                        apply_to_mask=False,
+                                        unit=unit)
+    
+    return newclip