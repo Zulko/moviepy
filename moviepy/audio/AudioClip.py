d0fbd73639488838ebacb41cec3bf658e758d3b7
Knut Ørland <git@humm.no>
2014-06-17 13:23:43 +0200

    Fixed some more logfile-related arguments

--- moviepy/audio/AudioClip.py
+++ moviepy/audio/AudioClip.py
@@ -91,7 +91,7 @@ class AudioClip(Clip):
     @requires_duration
     def to_audiofile(self,filename, fps=44100, nbytes=2,
                      buffersize=2000, codec='libvorbis',
-                     bitrate=None, verbose=True):
+                     bitrate=None, write_logfile=False, verbose=True):
         """ 
         codecs  = {        'libmp3lame': 'mp3',
                        'libvorbis':'ogg',
@@ -101,7 +101,7 @@ class AudioClip(Clip):
         """
                          
         return ffmpeg_audiowrite(self, filename, fps, nbytes, buffersize,
-                      codec=codec, bitrate=bitrate, verbose=verbose)
+                      codec=codec, bitrate=bitrate, write_logfile=write_logfile, verbose=verbose)
 
 
 class AudioArrayClip(AudioClip):
