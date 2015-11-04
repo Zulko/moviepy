21ffc4ae6855304e5ecc804b09e3710da72baf2f
Greg Humphreys <greg.humphreys@fanduel.com>
2015-11-04 18:38:05 +0000

    fix numerical precision bug in duration computation

--- moviepy/Clip.py
+++ moviepy/Clip.py
@@ -468,7 +468,7 @@ class Clip:
 
         def generator():
         
-            for t in np.arange(0, self.duration, 1.0/fps):
+            for t in np.arange(0, self.duration - 1.0/(2*fps), 1.0/fps):
         
                 frame = self.get_frame(t)
         
