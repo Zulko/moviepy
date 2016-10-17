33e29756a68ee3a74512c0f40016b09198907033
Andrew Wagner <andrew.wagner@robovision.eu>
2016-10-17 15:57:31 +0200

    Add a test case.

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,15 @@
 """
-Tests meant to be run with pytest
+To run the tests:
+    py.test tests.py
 """
-
-import pytest
 
 from moviepy.editor import *
 
-
-@pytest.fixture
-def example_video1():
-	
+#@pytest.fixture
+def test_if_TextClip_crashes():
+    overlay = TextClip(txt='foo',
+                       color='white',
+                       size=(640, 480),
+                       method='caption',
+                       align='center',
+                       fontsize=25)
