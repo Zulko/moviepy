7708ab8319d00485be0536489bf2ee2cc3bb2d68
AaronLai <aaronlaiya@icloud.com>
2016-12-08 16:38:10 +0800

    blink

--- moviepy/video/fx/blink.py
+++ moviepy/video/fx/blink.py
@@ -1,12 +1,31 @@
-def blink(clip, d_on, d_off):
+from copy import copy
+
+
+def blink(clip, d_on, d_off, start=None, end=None):
     """
     Makes the clip blink. At each blink it will be displayed ``d_on``
     seconds and disappear ``d_off`` seconds. Will only work in
     composite clips.
     """
     newclip = copy(clip)
+
+    if start is None:
+        start = 0
+
+    if end is None:
+        end = 10000
+
     if newclip.mask is None:
         newclip = newclip.with_mask()
+
     D = d_on + d_off
-    newclip.mask = newclip.mask.fl( lambda gf,t: gf(t)*((t % D) < d_on))
+
+    def fl_blink(gf, t):
+        if t >= start and t <= end:
+            return gf(t)*((t % D) < d_on)
+        else:
+            return gf(t)
+
+    newclip.mask = newclip.mask.fl(fl_blink)
+
     return newclip
