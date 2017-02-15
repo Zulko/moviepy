f4a460646f87b63781ad32b8ef6a0b9c0d8a6290
Billy Earney <billye@millerslab.com>
2017-02-15 14:21:53 -0600

    fix issue #357, which makes real problem more obvious (media file does not exist

--- moviepy/video/io/VideoFileClip.py
+++ moviepy/video/io/VideoFileClip.py
@@ -54,8 +54,8 @@ class VideoFileClip(VideoClip):
         
         # Make a reader
         pix_fmt= "rgba" if has_mask else "rgb24"
-        reader = FFMPEG_VideoReader(filename, pix_fmt=pix_fmt)
-        self.reader = reader
+        self.reader = None #need this just in case FFMPEG has issues (__del__ complains)
+        self.reader = FFMPEG_VideoReader(filename, pix_fmt=pix_fmt)
         # Make some of the reader's attributes accessible from the clip
         self.duration = self.reader.duration
         self.end = self.reader.duration
