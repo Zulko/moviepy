f8e8da0baeddaf7a3b725d301f2a6b9eeb6ec4c8
Billy Earney <billye@millerslab.com>
2017-02-15 15:25:39 -0600

    revert VideoFileClip.py back, take 2

--- moviepy/video/io/VideoFileClip.py
+++ moviepy/video/io/VideoFileClip.py
@@ -55,6 +55,7 @@ class VideoFileClip(VideoClip):
         # Make a reader
         pix_fmt= "rgba" if has_mask else "rgb24"
         reader = FFMPEG_VideoReader(filename, pix_fmt=pix_fmt)
+        self.reader = reader
         # Make some of the reader's attributes accessible from the clip
         self.duration = self.reader.duration
         self.end = self.reader.duration
