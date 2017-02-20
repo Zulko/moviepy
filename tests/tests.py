e899640b49f60d167d03c825f7698521f2c633cc
Billy Earney <billy.earney@gmail.com>
2017-02-20 13:54:28 -0600

    add test for issue 145

--- tests/tests.py
+++ tests/tests.py
@@ -9,4 +9,11 @@ from moviepy.editor import *
 
 @pytest.fixture
 def example_video1():
-	
+    pass
+
+_knights=VideoFileClip("media/knights.mp4")
+_knights10 = _knights.subclip(60,70)
+
+# issue 145
+with pytest.raises(Exception, message="Expecting Exception"):
+     _final = concatenation([_knights10], method = 'composite')
