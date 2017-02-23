9e6b9c825d444ec29e22f714ac92a33d968cfc17
Billy Earney <billy.earney@gmail.com>
2017-02-23 09:58:30 -0600

    add code to download python_logo.png

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -2,29 +2,30 @@
 Tests meant to be run with pytest
 """
 
-import sys
 import os
 import pytest
 
 from moviepy.editor import *
 
 
-#@pytest.fixture
-#def example_video1():
-#    pass
-
 global knights, knights10
 
-def download_youtube_video(url, filename):
+def download_url(url, filename):
     if not os.path.exists(filename):
        print("\nDownloading %s\n" % filename)
        download_webfile(url, filename)
        print("Downloading complete...\n")
 
+def download_youtube_video(youtube_id, filename):
+    download_url(youtube_id, filename)
 
 def test_download_media(capsys):
+    global knights, knights10
+
     with capsys.disabled():
        download_youtube_video("zvCvOC2VwDc", "media/knights.mp4")
+       download_url("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/260px-Python_logo_and_wordmark.svg.png",
+                    "media/python_logo.png")
 
     knights=VideoFileClip("media/knights.mp4")
     knights10 = knights.subclip(60,70)
