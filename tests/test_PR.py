0395b93cc0d06a5591c061a8c812b92cc50156ae
Tom Burrows <tomburrows13@btinternet.com>
2017-03-01 17:32:40 +0000

    Change

--- tests/test_PR.py
+++ tests/test_PR.py
@@ -39,7 +39,8 @@ def test_PR_424():
     # Uses `col` so should work the same as above, but give warning
     clip = ColorClip([1000, 600], col=(60, 60, 60), duration=10)
     # Should give 2 warnings and use `color`, not `col`
-    clip = ColorClip([1000, 600], color=(60, 60, 60), duration=10, col=(50,50,50))
+    clip = ColorClip([1000, 600], color=(60, 60, 60), duration=10, col=(2,2,2))
+
 
 if __name__ == '__main__':
    pytest.main()
