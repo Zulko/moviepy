a152c52c7ae3472c500c965618f76778026dce72
Billy Earney <billy.earney@gmail.com>
2017-03-02 10:00:45 -0600

    add ImageSequenceClip tests (#456)
    add test for ImageSequenceClip
    
    * update search path
    
    * remove undescore from local variables

--- tests/test_VideoFileClip.py
+++ tests/test_VideoFileClip.py
@@ -3,20 +3,20 @@ from moviepy.editor import *
 import os
 
 def test_setup():
-    _red = ColorClip((1024,800), col=(255,0,0))
-    _green = ColorClip((1024,800), col=(0,255,0))
-    _blue = ColorClip((1024,800), col=(0,0,255))
+    red = ColorClip((1024,800), col=(255,0,0))
+    green = ColorClip((1024,800), col=(0,255,0))
+    blue = ColorClip((1024,800), col=(0,0,255))
 
-    _red.fps=_green.fps=_blue.fps=30
-    _video = clips_array([[_red, _green, _blue]]).set_duration(5)
-    _video.write_videofile("/tmp/test.mp4")
+    red.fps=green.fps=blue.fps=30
+    video = clips_array([[red, green, blue]]).set_duration(5)
+    video.write_videofile("/tmp/test.mp4")
 
     assert os.path.exists("/tmp/test.mp4")
 
-    _clip = VideoFileClip("/tmp/test.mp4")
-    assert _clip.duration == 5
-    assert _clip.fps == 30
-    assert _clip.size == [1024*3, 800]
+    clip = VideoFileClip("/tmp/test.mp4")
+    assert clip.duration == 5
+    assert clip.fps == 30
+    assert clip.size == [1024*3, 800]
 
 if __name__ == '__main__':
    pytest.main()
--- tests/test_compositing.py
+++ tests/test_compositing.py
@@ -2,33 +2,33 @@ import pytest
 from moviepy.editor import *
 
 def test_clips_array():
-    _red = ColorClip((1024,800), col=(255,0,0))
-    _green = ColorClip((1024,800), col=(0,255,0))
-    _blue = ColorClip((1024,800), col=(0,0,255))
+    red = ColorClip((1024,800), col=(255,0,0))
+    green = ColorClip((1024,800), col=(0,255,0))
+    blue = ColorClip((1024,800), col=(0,0,255))
 
-    _video = clips_array([[_red, _green, _blue]])
+    video = clips_array([[red, green, blue]])
 
     with pytest.raises(ValueError,
                        message="Expecting ValueError (duration not set)"):
-       _video.resize(width=480).write_videofile("/tmp/test_clips_array.mp4")
+       video.resize(width=480).write_videofile("/tmp/test_clips_array.mp4")
 
 
 def test_clips_array():
-    _red = ColorClip((1024,800), col=(255,0,0))
-    _green = ColorClip((1024,800), col=(0,255,0))
-    _blue = ColorClip((1024,800), col=(0,0,255))
+    red = ColorClip((1024,800), col=(255,0,0))
+    green = ColorClip((1024,800), col=(0,255,0))
+    blue = ColorClip((1024,800), col=(0,0,255))
 
-    _video = clips_array([[_red, _green, _blue]]).set_duration(5)
+    video = clips_array([[red, green, blue]]).set_duration(5)
 
     with pytest.raises(AttributeError,
                        message="Expecting ValueError (fps not set)"):
-         _video.write_videofile("/tmp/test_clips_array.mp4")
+         video.write_videofile("/tmp/test_clips_array.mp4")
 
 
     #this one should work correctly
-    _red.fps=_green.fps=_blue.fps=30
-    _video = clips_array([[_red, _green, _blue]]).set_duration(5)
-    _video.write_videofile("/tmp/test_clips_array.mp4")
+    red.fps=green.fps=blue.fps=30
+    video = clips_array([[red, green, blue]]).set_duration(5)
+    video.write_videofile("/tmp/test_clips_array.mp4")
 
 
 if __name__ == '__main__':
