3fe39cb01d1cc55ebf46f987672dc0d8d7e73099
Billy Earney <billy.earney@gmail.com>
2017-03-02 09:14:27 -0600

    update search path

--- tests/test_ImageSequenceClip.py
+++ tests/test_ImageSequenceClip.py
@@ -2,8 +2,7 @@ import pytest
 from moviepy.editor import *
 
 import sys, os
-print(os.getcwd())
-sys.path.insert(0, os.path.abspath("."))
+sys.path.append("tests")
 import download_media
 
 def test_download_media(capsys):
