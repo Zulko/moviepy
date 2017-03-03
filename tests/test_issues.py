1d0cc2114a76b9e244aee46244943ce8d5c2ea48
Billy Earney <billy.earney@gmail.com>
2017-03-03 11:16:57 -0600

    add test for issue 368

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -21,6 +21,43 @@ def test_issue_145():
     with pytest.raises(Exception, message="Expecting Exception"):
          final = concatenate_videoclips([video], method = 'composite')
 
+def test_issue_368():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn import svm # sklearn = scikit-learn
+    from sklearn.datasets import make_moons
+    #from moviepy.editor import VideoClip
+    from moviepy.video.io.bindings import mplfig_to_npimage
+    import imageio
+
+    X, Y = make_moons(50, noise=0.1, random_state=2) # semi-random data
+
+    fig, ax = plt.subplots(1, figsize=(4, 4), facecolor=(1,1,1))
+    fig.subplots_adjust(left=0, right=1, bottom=0)
+    xx, yy = np.meshgrid(np.linspace(-2,3,500), np.linspace(-1,2,500))
+
+    def make_frame(t):
+        ax.clear()
+        ax.axis('off')
+        ax.set_title("SVC classification", fontsize=16)
+
+        classifier = svm.SVC(gamma=2, C=1)
+        # the varying weights make the points appear one after the other
+        weights = np.minimum(1, np.maximum(0, t**2+10-np.arange(50)))
+        classifier.fit(X, Y, sample_weight=weights)
+        Z = classifier.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=plt.cm.bone, alpha=0.8,
+                    vmin=-2.5, vmax=2.5, levels=np.linspace(-2,2,20))
+        ax.scatter(X[:,0], X[:,1], c=Y, s=50*weights, cmap=plt.cm.bone)
+
+        return mplfig_to_npimage(fig)
+
+    animation = VideoClip(make_frame,duration=2)
+    animation.write_gif("/tmp/svm.gif",fps=20)
+
+
+
 def test_issue_407():
     red = ColorClip((800, 600), color=(255,0,0)).set_duration(5)
     red.fps=30
