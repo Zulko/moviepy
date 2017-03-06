7d2c5b4e1f91648e95fc290b376ad534dd1c99c9
Billy Earney <billy.earney@gmail.com>
2017-03-06 09:10:19 -0600

    remove test for python 3.5 since tk error

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -26,8 +26,9 @@ def test_issue_368():
     if sys.version_info < (3,4):   #matplotlib only supported in python >= 3.4
        return
 
-    if sys.version_info == (3,5):  # $DISPLAY not set error in 3.5..
-       return                      # so ignore 3.5 for now
+    # $DISPLAY not set error in 3.5, so ignore 3.5 for now
+    if sys.version_info.major == 3 and sys.version_info.minor ==5:
+       return
 
     import numpy as np
     import matplotlib.pyplot as plt
