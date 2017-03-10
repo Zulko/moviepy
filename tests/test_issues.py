f9c65f04e484499ccb367a62f1793eaf0a44754f
Billy Earney <billy.earney@gmail.com>
2017-03-10 08:37:35 -0600

    Issue 359,  fix default opt argument to work with imageio and ImageMagick (#471)

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -29,6 +29,7 @@ def test_issue_285():
     merged_clip = concatenate_videoclips([clip_1, clip_2, clip_3])
     assert merged_clip.duration == 30
 
+    
 def test_issue_354():
     clip = ImageClip("media/python_logo.png")
 
@@ -40,6 +41,10 @@ def test_issue_354():
     fadecaption = clip.crossfadein(crosstime).crossfadeout(crosstime)
     ret = CompositeVideoClip([clip, fadecaption])
 
+def test_issue_359():
+    video = ColorClip((800, 600), color=(255,0,0)).set_duration(5)
+    video.fps=30
+    video.write_gif(filename="/tmp/issue_359.gif", tempfiles=True)
 
 def test_issue_407():
     red = ColorClip((800, 600), color=(255,0,0)).set_duration(5)
