f221a18b9d394e1aebcd78d15431f85287b9df63
Billy Earney <billy.earney@gmail.com>
2017-03-13 07:29:00 -0500

    fix Issue 368..  ValueError: Invalid value for quantizer: 'wu' (#460)
    Update gif_writers.py
    
    * fix nq error

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -28,7 +28,6 @@ def test_issue_285():
 
     merged_clip = concatenate_videoclips([clip_1, clip_2, clip_3])
     assert merged_clip.duration == 30
-
     
 def test_issue_354():
     clip = ImageClip("media/python_logo.png")
@@ -45,6 +44,48 @@ def test_issue_359():
     video = ColorClip((800, 600), color=(255,0,0)).set_duration(5)
     video.fps=30
     video.write_gif(filename="/tmp/issue_359.gif", tempfiles=True)
+
+def test_issue_368():
+    import sys
+    if sys.version_info < (3,4):   #matplotlib only supported in python >= 3.4
+       return
+
+    # $DISPLAY not set error in (travis-ci) python 3.5, so ignore 3.5 for now
+    if sys.version_info.major == 3 and sys.version_info.minor ==5:
+       return
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn import svm
+    from sklearn.datasets import make_moons
+    from moviepy.video.io.bindings import mplfig_to_npimage
+    import imageio
+
+    X, Y = make_moons(50, noise=0.1, random_state=2) # semi-random data
+
+    fig, ax = plt.subplots(1, figsize=(4, 4), facecolor=(1,1,1))
+    fig.subplots_adjust(left=0, right=1, bottom=0)
+    xx, yy = np.meshgrid(np.linspace(-2,3,500), np.linspace(-1,2,500))
+
+    def make_frame(t):
+        ax.clear()
+        ax.axis('off')
+        ax.set_title("SVC classification", fontsize=16)
+
+        classifier = svm.SVC(gamma=2, C=1)
+        # the varying weights make the points appear one after the other
+        weights = np.minimum(1, np.maximum(0, t**2+10-np.arange(50)))
+        classifier.fit(X, Y, sample_weight=weights)
+        Z = classifier.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=plt.cm.bone, alpha=0.8,
+                    vmin=-2.5, vmax=2.5, levels=np.linspace(-2,2,20))
+        ax.scatter(X[:,0], X[:,1], c=Y, s=50*weights, cmap=plt.cm.bone)
+
+        return mplfig_to_npimage(fig)
+
+    animation = VideoClip(make_frame,duration=2)
+    animation.write_gif("/tmp/svm.gif",fps=20)
 
 def test_issue_407():
     red = ColorClip((800, 600), color=(255,0,0)).set_duration(5)
