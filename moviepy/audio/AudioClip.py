80c3f999494a7997cfed590493174545578145b6
Will <will.power.mind.me@gmail.com>
2017-03-13 16:59:51 +0200

    Fix class name in doc string

--- moviepy/audio/AudioClip.py
+++ moviepy/audio/AudioClip.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 class AudioClip(Clip):
     """ Base class for audio clips.
     
-    See ``SoundClip`` and ``CompositeSoundClip`` for usable classes.
+    See ``AudioFileClip`` and ``CompositeSoundClip`` for usable classes.
     
     An AudioClip is a Clip with a ``make_frame``  attribute of
     the form `` t -> [ f_t ]`` for mono sound and
