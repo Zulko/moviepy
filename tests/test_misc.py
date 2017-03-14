3a975267ef8c27e5a6f8f7503da86f43468337e0
Billy Earney <billy.earney@gmail.com>
2017-03-14 11:06:28 -0500

    try blank generator

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -34,7 +34,8 @@ def test_subtitles():
        final.to_videofile("/tmp/subtitles1.mp4", fps=30)
     else:
        #travis-ci doesn't like TextClip
-       subtitles = SubtitlesClip("media/subtitles1.srt")
+       generator = lambda txt: pass
+       subtitles = SubtitlesClip("media/subtitles1.srt", generator)
 
     data = [([0.0, 4.0], 'Red!'), ([5.0, 9.0], 'More Red!'),
             ([10.0, 14.0], 'Green!'), ([15.0, 19.0], 'More Green!'),
