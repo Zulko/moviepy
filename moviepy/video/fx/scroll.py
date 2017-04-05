0948e7a25e79b01dae3c5b6cf9b0c272e2d196b7
Tom Burrows <tomburrows13@btinternet.com>
2017-04-05 22:17:50 +0100

    Add int() wrapper to prevent floats

--- moviepy/video/fx/scroll.py
+++ moviepy/video/fx/scroll.py
@@ -11,8 +11,8 @@ def scroll(clip, h=None, w=None, x_speed=0, y_speed=0,
     ymax = clip.h-h-1
 
     def f(gf,t):
-        x = max(0, min(xmax, x_start+ np.round(x_speed*t)))
-        y = max(0, min(ymax, y_start+ np.round(y_speed*t)))
+        x = int(max(0, min(xmax, x_start+ np.round(x_speed*t))))
+        y = int(max(0, min(ymax, y_start+ np.round(y_speed*t))))
         return gf(t)[y:y+h, x:x+w]
     
     return clip.fl(f, apply_to = apply_to)
