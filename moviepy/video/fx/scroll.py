d88850262534e38b52d9dc40bd897c4ce8830ca9
Gloin1313 <Gloin1313@users.noreply.github.com>
2017-04-14 14:17:03 +0100

    Merge pull request #528 from Gloin1313/scroll_fix
    Fixed `scroll` compatability with numpy 1.12

--- moviepy/video/fx/scroll.py
+++ moviepy/video/fx/scroll.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def scroll(clip, h=None, w=None, x_speed=0, y_speed=0,
            x_start=0, y_start=0, apply_to="mask",
            ease_func=None, period=1):
@@ -14,8 +12,8 @@ def scroll(clip, h=None, w=None, x_speed=0, y_speed=0,
     def f(gf,t):
         if ease_func and t < period:
             t = ease_func(t, duration=period) * period
-        x = max(0, min(xmax, x_start+ round(x_speed*t)))
-        y = max(0, min(ymax, y_start+ round(y_speed*t)))
+        x = int(max(0, min(xmax, x_start+ round(x_speed*t))))
+        y = int(max(0, min(ymax, y_start+ round(y_speed*t))))
         return gf(t)[y:y+h, x:x+w]
     
     return clip.fl(f, apply_to = apply_to)
