8f0f2b903fc1fe31a6ef619fe029b3d0648f9375
Billy Earney <billy.earney@gmail.com>
2017-04-17 07:45:24 -0500

    do not run TextClip tests if running on TRAVIS.

--- tests/test_TextClip.py
+++ tests/test_TextClip.py
@@ -3,15 +3,17 @@ import sys
 import pytest
 from moviepy.video.fx.blink import blink
 from moviepy.video.VideoClip import TextClip
+from test_helper import TMP_DIR, TRAVIS
 
 sys.path.append("tests")
 
 def test_duration():
-    return
     #TextClip returns the following error under Travis (issue with Imagemagick)
     #convert.im6: not authorized `@/tmp/tmpWL7I3M.txt' @ error/property.c/InterpretImageProperties/3057.
     #convert.im6: no images defined `PNG32:/tmp/tmpRZVqGQ.png' @ error/convert.c/ConvertImageCommand/3044.
-
+    if TRAVIS:
+       return
+    
     clip = TextClip('hello world', size=(1280,720), color='white')
     clip.set_duration(5)
     assert clip.duration == 5
@@ -22,12 +24,17 @@ def test_duration():
 
 # Moved from tests.py. Maybe we can remove these?
 def test_if_textclip_crashes_in_caption_mode():
+    if TRAVIS:
+       return
+    
     TextClip(txt='foo', color='white', size=(640, 480), method='caption',
              align='center', fontsize=25)
 
 def test_if_textclip_crashes_in_label_mode():
+    if TRAVIS:
+       return
+    
     TextClip(txt='foo', method='label')
-
 
 if __name__ == '__main__':
    pytest.main()
