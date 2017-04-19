47cf9ecfc8f2b93a45e4294ce94c1470909b825d
Billy Earney <billy.earney@gmail.com>
2017-04-19 14:58:02 -0500

    add ImageSequenceClip exception test

--- tests/test_ImageSequenceClip.py
+++ tests/test_ImageSequenceClip.py
@@ -27,5 +27,19 @@ def test_1():
     assert clip.duration == sum(durations)
     clip.write_videofile("/tmp/ImageSequenceClip1.mp4", fps=30)
 
+def test_2():
+    images=[]
+    durations=[]
+
+    durations.append(1)
+    images.append("media/python_logo.png")
+    durations.append(2)
+    images.append("media/matplotlib_demo1.png")
+
+    #images are not the same size..
+    with pytest.raises(Exception, message='Expecting Exception'):
+         ImageSequenceClip(images, durations=durations)
+
+
 if __name__ == '__main__':
    pytest.main()
