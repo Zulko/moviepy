44180dbb64f3870a8597c1bf7c1920645c103502
Alberto Galera <galerajimenez@gmail.com>
2017-05-05 11:47:36 +0200

    Cache actual range level and minor refactor

--- moviepy/audio/fx/audio_levels.py
+++ moviepy/audio/fx/audio_levels.py
@@ -11,55 +11,63 @@ def audio_levels(clip, levels, fast=False):
                        levels=[(0, 1), (5, 0), (10, 1)],
                        fast=True)  # fast is imprecise
     """
+    def get_range_levels(t):
+        if t >= cache['prv'][0] and t < cache['nxt'][0]:
+            return cache['prv'], cache['nxt'], cache['final']
+        elif t >= cache['nxt'][0] and cache['final']:
+            # final point not defined
+            return cache['prv'], cache['nxt'], cache['final']
+
+        match_next = False
+        for l in levels:
+            if l[0] <= t:
+                cache['prv'] = l
+            else:
+                cache['nxt'] = l
+                match_next = True
+                break
+        if not match_next:
+            cache['nxt'] = cache['prv'][0] + 1, cache['prv'][1]
+            cache['final'] = True
+
+        elif cache['prv'] == cache['nxt']:
+            # init point not defined
+            cache['prv'] = cache['prv'][0] - 1, cache['prv'][1]
+
+        return cache['prv'], cache['nxt'], cache['final']
 
     def level(gf, t):
-        def get_range_levels(t):
-            ant = levels[0]
-            sig = None
-            final = False
-
-            for l in levels:
-                if l[0] <= t:
-                    ant = l
-                else:
-                    sig = l
-                    break
-            if not sig:  # hack time
-                sig = ant[0] + 1, ant[1]
-                final = True
-            return ant, sig, final
-
         def scalar(t):
             gft = gf(t)
-            ant, sig, final = get_range_levels(t)
+            prv, nxt, final = get_range_levels(t)
 
-            pos = (t - ant[0]) / (sig[0] - ant[0])
-            factor = pos * sig[1] + (1 - pos) * ant[1]
+            pos = (t - prv[0]) / (nxt[0] - prv[0])
+            factor = pos * nxt[1] + (1 - pos) * prv[1]
             return np.array([factor, factor]) * gft
 
-        def not_scalar_fast(t, ant=False, sig=False):
+        def not_scalar_fast(t, prv=False, nxt=False):
             gft = gf(t)
 
-            if not ant or not sig:
-                ant, sig, final = get_range_levels(t[0])
+            if not prv or not nxt:
+                prv, nxt, final = get_range_levels(t[0])
 
-            pos = (t - ant[0]) / (sig[0] - ant[0])
-            factor = pos * sig[1] + (1 - pos) * ant[1]
+            pos = (t - prv[0]) / (nxt[0] - prv[0])
+            factor = pos * nxt[1] + (1 - pos) * prv[1]
             return np.vstack([factor, factor]).T * gft
 
         def not_scalar(t):
             gft = gf(t)
-            ant, sig, final = get_range_levels(t[0])
+            prv, nxt, final = get_range_levels(t[0])
 
-            if not t[-1] >= sig[0] or final:
-                return not_scalar_fast(t, ant, sig)
+            if t[-1] < nxt[0] or final:
+                return not_scalar_fast(t, prv, nxt)
 
             for it in range(len(t)):
-                if t[it] >= sig[0] and not final:
-                    ant, sig, final = get_range_levels(t[it])
+                if t[it] >= nxt[0] and not final:
+                    prv, nxt, final = get_range_levels(t[it])
 
-                pos = (t[it] - ant[0]) / (sig[0] - ant[0])
-                factor = pos * sig[1] + (1 - pos) * ant[1]
+                pos = (t[it] - prv[0]) / (nxt[0] - prv[0])
+                factor = pos * nxt[1] + (1 - pos) * prv[1]
                 gft[it] *= np.array([factor, factor])
             return gft
 
@@ -69,4 +77,6 @@ def audio_levels(clip, levels, fast=False):
             return np.array(not_scalar_fast(t))
         return np.array(not_scalar(t))
 
+    cache = {'prv': levels[0], 'nxt': levels[0], 'final': False}
+    get_range_levels(0)
     return clip.fl(level, keep_duration=True)
