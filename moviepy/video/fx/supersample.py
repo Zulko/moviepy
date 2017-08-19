5625d33f286229074f6ecb96093b00e360a192a7
inkeye <tim.pauli@posteo.de>
2017-08-19 02:43:40 +0200

    fixes nframes==1. also little semantic shift. now every frame really uses exactly n frames for avg. before edge cases where not. improvement for #634

--- moviepy/video/fx/supersample.py
+++ moviepy/video/fx/supersample.py
@@ -5,8 +5,9 @@ def supersample(clip, d, nframes):
     taken in the interval [t-d, t+d]. This results in motion blur."""
 
     def fl(gf, t):
-        tt = np.linspace(t-d, t+d, nframes)
-        tt = filter(lambda x: x >= 0, tt)
+        lower_bound = t - d
+        lower_bound = 0 if lower_bound < 0 else lower_bound
+        tt = np.linspace(lower_bound, t+d, nframes)
         avg = np.mean(1.0*np.array([gf(t_) for t_ in tt], dtype='uint16'), axis=0)
         return avg.astype("uint8")
 
