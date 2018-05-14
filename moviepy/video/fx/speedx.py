b7c88379e7eb74cd7488ca7171a199af57fe139c
Jens Kouros <jens.kouros@gmail.com>
2018-05-14 19:36:12 +0000

    Implement function to stretch audio using librosa

--- moviepy/video/fx/speedx.py
+++ moviepy/video/fx/speedx.py
@@ -1,4 +1,18 @@
 from moviepy.decorators import apply_to_mask,apply_to_audio
+from moviepy.audio.AudioClip import AudioArrayClip
+from librosa.effects import time_stretch
+
+
+def _stretch_audio(audioclip, factor):
+    """Stretches audioclip by factor."""
+    sound_array = audioclip.to_soundarray()
+
+    if audioclip.nchannels == 1:
+        stretched_array = time_stretch(sound_array, factor)
+    else
+        stretched_array = np.transpose( np.array([time_stretch(sound_array[:,0]), time_stretch(sound_array[:,1])]) )
+
+    return AudioArrayClip(stretched_array, fps=44100)
 
 
 def speedx(clip, factor = None, final_duration=None):
@@ -9,13 +23,13 @@ def speedx(clip, factor = None, final_duration=None):
     computed.
     The same effect is applied to the clip's audio and mask if any.
     """
-    
+
     if final_duration:
         factor = 1.0* clip.duration / final_duration
-        
+
     newclip = clip.fl_time(lambda t: factor * t, apply_to=['mask', 'audio'])
-    
+
     if clip.duration is not None:
         newclip = newclip.set_duration(1.0 * clip.duration / factor)
-    
+
     return newclip
