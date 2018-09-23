91037cf22cc7033387e64a33f81a691268e855de
Jens Kouros <jens.kouros@gmail.com>
2018-09-23 21:54:05 +0000

    Add audio time stretching to video/fx/speedx.py
    Give users the option to stretch the audio when changing the speed of a video. The default is False, so that all existing uses should still work as usual.

--- moviepy/video/fx/speedx.py
+++ moviepy/video/fx/speedx.py
@@ -1,7 +1,7 @@
 from moviepy.decorators import apply_to_mask,apply_to_audio
+from moviepy.audio.fx.time_stretch import time_stretch
 
-
-def speedx(clip, factor = None, final_duration=None):
+def speedx(clip, factor = None, final_duration=None, stretch_audio=False):
     """
     Returns a clip playing the current clip but at a speed multiplied
     by ``factor``. Instead of factor one can indicate the desired
@@ -9,13 +9,18 @@ def speedx(clip, factor = None, final_duration=None):
     computed.
     The same effect is applied to the clip's audio and mask if any.
     """
-    
+
     if final_duration:
         factor = 1.0* clip.duration / final_duration
-        
+
     newclip = clip.fl_time(lambda t: factor * t, apply_to=['mask', 'audio'])
-    
+
     if clip.duration is not None:
         newclip = newclip.set_duration(1.0 * clip.duration / factor)
-    
+
+    if stretch_audio:
+        stretched_audio = time_stretch(clip.audio, factor)
+        stretched_audio.duration = newclip.duration
+        newclip = newclip.set_audio(stretched_audio)
+
     return newclip
