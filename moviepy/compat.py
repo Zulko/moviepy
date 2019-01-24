765accbd2669347eedd1e2ba2a2f6560d8baebba
PaperBag <ben.zichri@gmail.com>
2019-01-24 18:57:50 +0200

    add fspath function to compat module

--- moviepy/compat.py
+++ moviepy/compat.py
@@ -11,3 +11,23 @@ try:
     from subprocess import DEVNULL    # Python 3
 except ImportError:
     DEVNULL = open(os.devnull, 'wb')  # Python 2
+
+try:
+    from os import fspath             # Python 3.6+
+except ImportError:
+    try:
+        import pathlib
+    except ImportError:
+        
+        def fspath(path):             # Python 3.3-
+            if isinstance(path, string_types):
+                return path
+            raise TypeError("expected string object, "
+                            "not %s" % path.__class__.__name__)
+    else:
+        
+        def fspath(path):             # Python 3.4-3.5
+            if isinstance(path, (string_types, pathlib.Path)):
+                return str(path)
+            raise TypeError("expected string or Path object, "
+                            "not %s" % path.__class__.__name__)
