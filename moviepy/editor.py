921594337373223d545cb45db049cb53fc4d4024
Zulko <Zulko@users.noreply.github.com>
2019-02-06 22:21:23 +0000

    typo in editor.py

--- moviepy/editor.py
+++ moviepy/editor.py
@@ -25,7 +25,7 @@ import imageio
 # Checks to see if the user has set a place for their own version of ffmpeg
 
 if os.getenv('FFMPEG_BINARY', 'ffmpeg-imageio') == 'ffmpeg-imageio':
-    if sys.version_info[0] < (3, 4):
+    if sys.version_info < (3, 4):
         #uses an old version of imageio with ffmpeg.download.
         imageio.plugins.ffmpeg.download()
 
