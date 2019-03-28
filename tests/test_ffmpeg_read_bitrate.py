6e3b54a9bcb52e43c7b81e674ddcac3db8d8f57c
crazyxw <crazy_wish@163.com>
2019-03-28 09:37:57 +0800

    Many fixes changes for faster tests - hopefully no regression

--- tests/test_ffmpeg_read_bitrate.py
+++ tests/test_ffmpeg_read_bitrate.py
@@ -3,11 +3,10 @@ from moviepy.video.io.ffmpeg_reader import ffmpeg_parse_infos
 import pytest
 import sys
 sys.path.append("tests")
-media = "D://1.mp4"
 
 
 def test_one():
-    infos = ffmpeg_parse_infos(media)
+    infos = ffmpeg_parse_infos("tests/resource/1.mp4")
     assert infos["audio_bitrate"]
     assert infos["video_bitrate"]
 
