58da25e9926b327c6fb8dd99ee3797dd4d885db6
Gordon Pendleton <wgordonw1@gmail.com>
2019-11-17 21:25:56 -0500

    use flv output format for mp3 compatibility

--- tests/test_AudioClips.py
+++ tests/test_AudioClips.py
@@ -93,7 +93,7 @@ def test_write_audiofile_with_silent_video():
     d=ffmpeg_parse_infos(source_video)
     assert not d['audio_found']
     clip = AudioFileClip("media/crunching.mp3")
-    location = os.path.join(TMP_DIR, "test_write_audiofile_with_silent_video.ogg")
+    location = os.path.join(TMP_DIR, "test_write_audiofile_with_silent_video.flv")
     clip.write_audiofile(location)
     d=ffmpeg_parse_infos(location)
     assert d['audio_found']
