8670b4b8ee25aed7e7a979c7d5a0aad50d4f7e9f
jwg4 <jwg4@users.noreply.github.com>
2020-01-16 01:39:39 +0000

    Change the frame we look for.

--- tests/test_VideoFileClip.py
+++ tests/test_VideoFileClip.py
@@ -50,7 +50,13 @@ def test_shallow_copy():
     video = VideoFileClip(video_file)
     video_copy = video.set_start(1)
     del(video_copy)
-    video.audio.make_frame(2)
+    # The clip object buffers 200000 frames, around 5 seconds ahead.
+    # When recentering the buffer, if the new buffer is more than 1000000 frames, around 25s
+    # ahead of the end of the current buffer, the reader will reinitialize and fix self.proc
+    # Thus to trigger the bug, you have to look for a frame between ~5 and ~30 seconds away.
+    # These numbers might vary for different reasons and it would be nice to have a test
+    # which was robust to changes in default buffer size, etc.
+    video.audio.make_frame(15)
 
 
 if __name__ == '__main__':
