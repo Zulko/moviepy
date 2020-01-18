1844d2084ec2cfd6ec48afb88348190c414de244
Bernd Feige <Bernd.Feige@gmx.net>
2020-01-18 13:55:06 +0100

    Added current and more general examples/headblur.py

--- examples/headblur.py
+++ examples/headblur.py
@@ -1,51 +1,50 @@
+#!/usr/bin/env python
+
+# https://zulko.github.io/moviepy/examples/headblur.html
+
+import sys
+import pathlib
 import pickle
+import moviepy.editor
+import moviepy.video.tools.tracking
 
-from moviepy.editor import *
-from moviepy.video.tools.tracking import manual_tracking, to_fxfy
+# Two arguments: movie_in [subclip_s]
+# You can try (for checking the size of the blur) by setting subclip_s=1
+# Then delete the .dat file and do the whole file without second argument
+movie_in=sys.argv[1]
+if len(sys.argv)==3:
+ subclip_s=float(sys.argv[2])
+else:
+ subclip_s=None
 
-
-# LOAD THE CLIP (subclip 6'51 - 7'01 of a chaplin movie)
-clip = VideoFileClip("../../videos/chaplin.mp4").subclip((6,51.7),(7,01.3))
+trackingfile=pathlib.Path(movie_in+'.dat')
+# Often a subclip (min,sec) is wanted, and if it's only for testing...
+clip = moviepy.editor.VideoFileClip(movie_in)
+if subclip_s is not None:
+ clip=clip.subclip((0,0),(0,subclip_s))
 
 # MANUAL TRACKING OF THE HEAD
 
-# the three next lines are for the manual tracking and its saving
-# to a file, it must be commented once the tracking has been done
-# (after the first run of the script for instance).
-# Note that we save the list (ti,xi,yi), not the functions fx and fy
-# (that we will need) because they have dependencies.
-
-#txy, (fx,fy) = manual_tracking(clip, fps=6)
-#with open("../../chaplin_txy.dat",'w+') as f:
-#    pickle.dump(txy)
-
-
+# The next lines are for the manual tracking and its saving
+# to a file, which is done if tracking.dat does not yet exist.
+if not trackingfile.exists():
+ # This is a list of moviepy.video.tools.interpolators.Trajectory
+ tracking=moviepy.video.tools.tracking.manual_tracking(clip, fps=6)[0]
+ with trackingfile.open('wb') as f:
+  pickle.dump(tracking,f)
 
 # IF THE MANUAL TRACKING HAS BEEN PREVIOUSLY DONE,
 # LOAD THE TRACKING DATA AND CONVERT IT TO FUNCTIONS x(t),fy(t)
 
-with open("../../chaplin_txy.dat",'r') as f:
-    fx,fy = to_fxfy( pickle.load(f) )
+with trackingfile.open('rb') as f:
+ tracking = pickle.load(f)
 
+# BLUR HEAD IN THE CLIP
 
-# BLUR CHAPLIN'S HEAD IN THE CLIP
+# /usr/lib/python3.7/site-packages/moviepy/video/fx/headblur.py
+# r_zone is the radius and r_blur the intensity of the blurring -
+# r_blur must be integer but is set to 2*r_zone/3 in the code if unset,
+# creating a float leading to an error, so rather set it explicitly here...
+clip_blurred = clip.fx(moviepy.editor.vfx.headblur, tracking.xi, tracking.yi, 50, 20)
 
-clip_blurred = clip.fx( vfx.headblur, fx, fy, 25)
-
-
-# Generate the text, put in on a grey background
-
-txt = TextClip("Hey you ! \n You're blurry!", color='grey70',
-               size = clip.size, bg_color='grey20',
-               font = "Century-Schoolbook-Italic", fontsize=40)
-               
-               
-# Concatenate the Chaplin clip with the text clip, add audio
-
-final = concatenate_videoclips([clip_blurred,txt.set_duration(3)]).\
-          set_audio(clip.audio)
-
-# We write the result to a file. Here we raise the bitrate so that
-# the final video is not too ugly.
-
-final.write_videofile('../../blurredChaplin.avi', bitrate="3000k")
+clip_blurred.write_videofile(movie_in+'_blurred.mp4', bitrate="3000k")
