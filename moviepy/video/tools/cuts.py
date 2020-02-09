878c86c42f51b783e21c375edabd6948447867d6
Martín Gaitán <gaitan@gmail.com>
2020-02-09 13:20:25 -0300

    space after operators

--- moviepy/video/tools/cuts.py
+++ moviepy/video/tools/cuts.py
@@ -9,9 +9,8 @@ import numpy as np
 def find_video_period(clip,fps=None,tmin=.3):
     """ Finds the period of a video based on frames correlation """
     
-
     frame = lambda t: clip.get_frame(t).flatten()
-    tt = np.arange(tmin,clip.duration,1.0/ fps)[1:]
+    tt = np.arange(tmin, clip.duration, 1.0/ fps)[1:]
     ref = frame(0)
     corrs = [ np.corrcoef(ref, frame(t))[0,1] for t in tt]
     return tt[np.argmax(corrs)]
