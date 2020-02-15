bbc65be6bba571609e4bef7d778c207058dd4edb
Martín Gaitán <gaitan@gmail.com>
2020-02-15 01:26:16 -0300

    factorize skip as a decorator

--- tests/test_AudioClips.py
+++ tests/test_AudioClips.py
@@ -11,10 +11,13 @@ from moviepy.audio.AudioClip import AudioClip, concatenate_audioclips, Composite
 
 from .test_helper import TMP_DIR
 
-def test_audio_coreader():
-    if sys.platform.startswith("win"):
-        pytest.skip("Temporarily skipping on windows because otherwise test suite fails with Invalid Handle Error")
+skip_if_windows = pytest.mark.skipif(
+    sys.platform.startswith("win"),
+    reason="Temporarily skipping on windows because otherwise test suite fails with Invalid Handle Error"
+)
 
+@skip_if_windows
+def test_audio_coreader():
     sound = AudioFileClip("media/crunching.mp3")
     sound = sound.subclip(1, 4)
     sound2 = AudioFileClip("media/crunching.mp3")
@@ -26,9 +29,6 @@ def test_audioclip():
     clip.write_audiofile(os.path.join(TMP_DIR, "audioclip.mp3"))
 
 def test_audioclip_concat():
-    if sys.platform.startswith("win"):
-        pytest.skip("Temporarily skipping on windows because otherwise test suite fails with Invalid Handle Error")
-
     make_frame_440 = lambda t: [sin(440 * 2 * pi * t)]
     make_frame_880 = lambda t: [sin(880 * 2 * pi * t)]
 
@@ -46,10 +46,8 @@ def test_audioclip_concat():
     concat_clip.write_audiofile(os.path.join(TMP_DIR, "concat_audioclip.mp3"))
 
 
+@skip_if_windows
 def test_audioclip_with_file_concat():
-    if sys.platform.startswith("win"):
-        pytest.skip("Temporarily skipping on windows because otherwise test suite fails with Invalid Handle Error")
-
     make_frame_440 = lambda t: [sin(440 * 2 * pi * t)]
     clip1 = AudioClip(make_frame_440, duration=1, fps=44100)
 
@@ -65,9 +63,6 @@ def test_audioclip_with_file_concat():
 
 
 def test_audiofileclip_concat():
-    if sys.platform.startswith("win"):
-        pytest.skip("Temporarily skipping on windows because otherwise test suite fails with Invalid Handle Error")
-
     sound = AudioFileClip("media/crunching.mp3")
     sound = sound.subclip(1, 4)
 
@@ -76,6 +71,7 @@ def test_audiofileclip_concat():
     concat = concatenate_audioclips((sound, sound2))
 
     concat.write_audiofile(os.path.join(TMP_DIR, "concat_audio_file.mp3"))
+
 
 if __name__ == "__main__":
     pytest.main()
