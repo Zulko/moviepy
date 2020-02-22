8c9fdae0a39c7b00d35667830af3e55c0ffb19a3
Tom Burrows <tomburrows13@btinternet.com>
2020-02-22 17:38:57 +0000

    Simplify editor.py reading environment variable

--- moviepy/editor.py
+++ moviepy/editor.py
@@ -24,7 +24,7 @@ import sys
 import imageio
 # Checks to see if the user has set a place for their own version of ffmpeg
 
-if os.getenv('FFMPEG_BINARY', 'ffmpeg-imageio') == 'ffmpeg-imageio':
+if os.getenv('FFMPEG_BINARY') is None:
     if sys.version_info < (3, 4):
         #uses an old version of imageio with ffmpeg.download.
         imageio.plugins.ffmpeg.download()
