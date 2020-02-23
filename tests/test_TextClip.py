e70469522b05680dd2f05e49af10d5194a0c3ba9
Tom Burrows <tomburrows13@btinternet.com>
2020-02-23 04:24:54 +0000

    Neatening up

--- tests/test_TextClip.py
+++ tests/test_TextClip.py
@@ -6,34 +6,33 @@ from moviepy.utils import close_all_clips
 from moviepy.video.fx.blink import blink
 from moviepy.video.VideoClip import TextClip
 
-from .test_helper import TMP_DIR, FONT
+from .test_helper import FONT
 
 
 def test_fontlist():
-    raise ValueError(TextClip.list('font'))
+    TextClip.list('font')
 
-@pytest.mark.timeout(30)
+
 def test_duration():
-
     clip = TextClip('hello world', size=(1280,720), color='white', font=FONT)
-    clip = clip.set_duration(5) #  Changed due to #598.
+    clip = clip.set_duration(5)
     assert clip.duration == 5
     clip.close()
 
-    clip2 = clip.fx(blink, d_on=1, d_off=1, font=FONT)
+    clip2 = clip.fx(blink, d_on=1, d_off=1)
     clip2 = clip2.set_duration(5)
     assert clip2.duration == 5
     close_all_clips(locals())
 
+
 # Moved from tests.py. Maybe we can remove these?
-@pytest.mark.timeout(30)
 def test_if_textclip_crashes_in_caption_mode():
     TextClip(txt='foo', color='white', size=(640, 480), method='caption',
              align='center', fontsize=25, font=FONT).close()
 
-@pytest.mark.timeout(30)
+
 def test_if_textclip_crashes_in_label_mode():
-    TextClip(txt='foo', method='label').close()
+    TextClip(txt='foo', method='label', font=FONT).close()
 
 
 if __name__ == '__main__':
