9f35019e7e2fd4567018f61ef1c178d312cc1252
Tom Burrows <tomburrows13@btinternet.com>
2020-02-23 04:48:57 +0000

    Fix requirements syntax error

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ requires = [
     "imageio_ffmpeg>=0.2.0; python_version>='3.4'",
     'tqdm>=4.11.2,<5.0',
     "numpy>=1.17.3; python_version!='2.7'",
-    "numpy; python_version>='2.7'"
+    "numpy; python_version>='2.7'",
     'requests>=2.8.1,<3.0',
     'proglog<=1.0.0'
     ]
