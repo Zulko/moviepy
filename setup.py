6d8db8436b4e4fcde45d741a52e9ff50422f8525
Tom Burrows <tomburrows13@btinternet.com>
2020-03-24 17:38:18 +0000

    Remove support for Python 3.5 (#1106)
    Agreed in #1081.

--- setup.py
+++ setup.py
@@ -120,7 +120,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
