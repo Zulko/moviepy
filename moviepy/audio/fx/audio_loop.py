43023034527f6a0a6625a55273afcd9c29bd007b
Martín Gaitán <gaitan@gmail.com>
2020-03-23 23:46:18 -0300

    audio loop support videoclip instance

--- moviepy/audio/fx/audio_loop.py
+++ moviepy/audio/fx/audio_loop.py
@@ -1,7 +1,7 @@
 from ..AudioClip import concatenate_audioclips
 
 
-def audio_loop(audioclip, nloops=None, duration=None):
+def audio_loop(clip, nloops=None, duration=None):
     """ Loops over an audio clip.
 
     Returns an audio clip that plays the given clip either
@@ -17,12 +17,17 @@ def audio_loop(audioclip, nloops=None, duration=None):
     >>> videoclip.set_audio(audio)
 
     """
+    try:
+        clip = clip.audio
+    except AttributeError:
+        # assume it's a an audioclip
+        pass
 
     if duration is not None:
 
-        nloops = int( duration/ audioclip.duration)+1
-        return concatenate_audioclips(nloops*[audioclip]).set_duration(duration)
+        nloops = int( duration/ clip.duration)+1
+        return concatenate_audioclips(nloops*[clip]).set_duration(duration)
     
     else:
 
-        return concatenate_audioclips(nloops*[audioclip])
+        return concatenate_audioclips(nloops*[clip])
