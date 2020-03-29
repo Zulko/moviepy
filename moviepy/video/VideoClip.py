e22f695786c3d67acdfabe40193ada41e8a9162c
Martín Gaitán <gaitan@gmail.com>
2020-03-29 00:14:57 -0300

    remove col from ColorClip class

--- moviepy/video/VideoClip.py
+++ moviepy/video/VideoClip.py
@@ -1052,29 +1052,13 @@ class ColorClip(ImageClip):
     ismask
       Set to true if the clip will be used as a mask.
 
-    col
-      Has been deprecated. Do not use.
     """
 
-    def __init__(self, size, color=None, ismask=False, duration=None, col=None):
-        if col is not None:
-            warnings.warn(
-                "The `ColorClip` parameter `col` has been deprecated."
-                " Please use `color` instead.",
-                DeprecationWarning,
-            )
-            if color is not None:
-                warnings.warn(
-                    "The arguments `color` and `col` have both been "
-                    "passed to `ColorClip` so `col` has been ignored.",
-                    UserWarning,
-                )
-            else:
-                color = col
+    def __init__(self, size, color=None, ismask=False, duration=None):
         w, h = size
         shape = (h, w) if np.isscalar(color) else (h, w, len(color))
-        ImageClip.__init__(
-            self, np.tile(color, w * h).reshape(shape), ismask=ismask, duration=duration
+        super().__init__(
+            np.tile(color, w * h).reshape(shape), ismask=ismask, duration=duration
         )
 
 
