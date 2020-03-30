326a64494253fed5a6f2b8a07a2f8c09dc99663f
Nick B <nick@declension.net>
2020-03-30 08:41:55 +0100

    Fix broken parentheses

--- moviepy/video/io/ffmpeg_writer.py
+++ moviepy/video/io/ffmpeg_writer.py
@@ -146,7 +146,7 @@ class FFMPEG_VideoWriter:
             error = str(err) + (
                 "\n\nMoviePy error: FFMPEG encountered "
                 "the following error while writing file %s:"
-                "\n\n %s" % (self.filename, str(ffmpeg_error))
+                "\n\n %s" % (self.filename, str(ffmpeg_error)))
               
             if b"Unknown encoder" in ffmpeg_error:
 
