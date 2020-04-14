a1b7565454d94d2171705eda948f88985e6ef713
Tom Burrows <tomburrows13@btinternet.com>
2020-04-14 16:41:32 +0100

    Update rotate.py

--- moviepy/video/fx/rotate.py
+++ moviepy/video/fx/rotate.py
@@ -8,6 +8,7 @@ try:
     PIL_FOUND = True
 
     def pil_rotater(pic, angle, resample, expand):
+        # Ensures that pic is of the correct type
         return np.array(
             Image.fromarray(np.array(pic).astype(np.uint8)).rotate(
                 angle, expand=expand, resample=resample
