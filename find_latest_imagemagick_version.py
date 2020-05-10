f17f3aec9c1b0dc8bbda299a4fad1f07fbdce55e
Tom Burrows <tomburrows13@icloud.com>
2020-05-10 11:34:11 +0100

    Update CI ImageMagick version
    People will be using ImageMagick v7 with moviepy, so we should test against it..

--- find_latest_imagemagick_version.py
+++ find_latest_imagemagick_version.py
@@ -4,18 +4,18 @@ from urllib import request
 url = "https://legacy.imagemagick.org/script/index.php"
 
 """This little script parses url above to extract latest image magick version
-(major version 6.9), to feed it into CI system. Not the best way for reproducible
+(major version 7.0), to feed it into CI system. Not the best way for reproducible
 builds, but it's preferred for now over storing imagemagick installer into the
 git repository
 """
 
 response = request.urlopen(url)
 html = response.read().decode(r"utf-8")
-r = re.compile(r"6\.9\.[0-9]+-[0-9]+")
+r = re.compile(r"7\.0\.[0-9]+-[0-9]+")
 version = r.findall(html)
 if len(version) == 0:
     raise ValueError(
-        "Could not find latest legacy 6.9.X-Y ImageMagick version from {}".format(url)
+        "Could not find latest legacy 7.0.X-Y ImageMagick version from {}".format(url)
     )
 version = version[0]
 # Append Q16 build
