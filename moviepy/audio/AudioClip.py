936d5405f2cba4f77e4c9b0b0d4e53226f9bb09f
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-01-12 13:07:51 +0100

    Fix 'AudioClip' docstring example

--- moviepy/audio/AudioClip.py
+++ moviepy/audio/AudioClip.py
@@ -37,8 +37,8 @@ class AudioClip(Clip):
 
     >>> # Plays the note A (a sine wave of frequency 440HZ)
     >>> import numpy as np
-    >>> make_frame = lambda t: 2*[ np.sin(440 * 2 * np.pi * t) ]
-    >>> clip = AudioClip(make_frame, duration=5)
+    >>> make_frame = lambda t: np.sin(440 * 2 * np.pi * t)
+    >>> clip = AudioClip(make_frame, duration=5, fps=44100)
     >>> clip.preview()
 
     """
