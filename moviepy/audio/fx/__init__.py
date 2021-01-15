28206d7fb00daf4d7cb85666760b405e7d68a685
Tom Burrows <tomburrows13@icloud.com>
2021-01-15 23:24:11 +0000

    Remove audio_loop (redundant because of loop)

--- moviepy/audio/fx/__init__.py
+++ moviepy/audio/fx/__init__.py
@@ -2,7 +2,6 @@
 
 from .audio_fadein import audio_fadein
 from .audio_fadeout import audio_fadeout
-from .audio_loop import audio_loop
 from .audio_normalize import audio_normalize
 from .multiply_stereo_volume import multiply_stereo_volume
 from .multiply_volume import multiply_volume
