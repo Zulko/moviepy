0cbe6d3c3a097ed3460aedb9d14730b57879057c
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-01-22 21:31:24 +0100

    Remove indentation from examples in function

--- moviepy/tools.py
+++ moviepy/tools.py
@@ -51,22 +51,22 @@ def convert_to_seconds(time):
     If the type of `time` is not valid,
     it's returned as is.
 
-    Here are the accepted formats::
+    Here are the accepted formats:
 
-        >>> convert_to_seconds(15.4)   # seconds
-        15.4
-        >>> convert_to_seconds((1, 21.5))   # (min,sec)
-        81.5
-        >>> convert_to_seconds((1, 1, 2))   # (hr, min, sec)
-        3662
-        >>> convert_to_seconds('01:01:33.045')
-        3693.045
-        >>> convert_to_seconds('01:01:33,5')    # coma works too
-        3693.5
-        >>> convert_to_seconds('1:33,5')    # only minutes and secs
-        99.5
-        >>> convert_to_seconds('33.5')      # only secs
-        33.5
+    >>> convert_to_seconds(15.4)   # seconds
+    15.4
+    >>> convert_to_seconds((1, 21.5))   # (min,sec)
+    81.5
+    >>> convert_to_seconds((1, 1, 2))   # (hr, min, sec)
+    3662
+    >>> convert_to_seconds('01:01:33.045')
+    3693.045
+    >>> convert_to_seconds('01:01:33,5')    # coma works too
+    3693.5
+    >>> convert_to_seconds('1:33,5')    # only minutes and secs
+    99.5
+    >>> convert_to_seconds('33.5')      # only secs
+    33.5
     """
     factors = (1, 60, 3600)
 
