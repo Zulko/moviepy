e73336d147fcfdf34036c3c5f34d58ef20f99a70
Jens Kouros <jens.kouros@posteo.de>
2021-04-15 22:26:46 +0200

    Reformat time_stretch.py using black

--- moviepy/audio/fx/time_stretch.py
+++ moviepy/audio/fx/time_stretch.py
@@ -1,8 +1,12 @@
-from moviepy.audio.AudioClip import AudioArrayClip
 from numpy import transpose, array
 from librosa.effects import time_stretch as lr_time_stretch
 
 
+from moviepy.audio.AudioClip import AudioArrayClip
+from moviepy.decorators import audio_video_fx
+
+
+@audio_video_fx
 def time_stretch(audioclip, factor):
     """Stretches audioclip by factor."""
     snd_array = audioclip.to_soundarray()
@@ -11,7 +15,12 @@ def time_stretch(audioclip, factor):
         stretched_array = lr_time_stretch(snd_array, factor)
     else:
         stretched_array = transpose(
-          array([lr_time_stretch(snd_array[:, 0], factor),
-                 lr_time_stretch(snd_array[:, 1], factor)]))
+            array(
+                [
+                    lr_time_stretch(snd_array[:, 0], factor),
+                    lr_time_stretch(snd_array[:, 1], factor),
+                ]
+            )
+        )
 
     return AudioArrayClip(stretched_array, fps=44100)
