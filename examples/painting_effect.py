274b077cd74d4ecc348d28333d83fc8aecbc2acd
Jon Furtado <54950706+JonathanFurtado@users.noreply.github.com>
2021-05-04 02:47:38 +0000

    Fix for Issue #134
    The group's fix for issue #134, we modified the code example to reflect the new changes within moviepy. Still need to figure out how to successfully edit the position of the added text on the screen.

--- examples/painting_effect.py
+++ examples/painting_effect.py
@@ -2,25 +2,23 @@
 
 from moviepy import *
 
-
 # WE TAKE THE SUBCLIPS WHICH ARE 2 SECONDS BEFORE & AFTER THE FREEZE
 
-charade = VideoFileClip("../../videos/charade.mp4")
-tfreeze = convert_to_seconds(19.21)  # Time of the freeze, 19'21
+test_video = VideoFileClip("media/audrey.mp4")
+tfreeze = convert_to_seconds(3)  # Time of the freeze, 3 seconds
 
-clip_before = charade.subclip(tfreeze - 2, tfreeze)
-clip_after = charade.subclip(tfreeze, tfreeze + 2)
-
+clip_before = test_video.subclip(tfreeze - 2, tfreeze)
+clip_after = test_video.subclip(tfreeze, tfreeze + 2)
 
 # THE FRAME TO FREEZE
 
-im_freeze = charade.to_ImageClip(tfreeze)
-painting = charade.fx(vfx.painting, saturation=1.6, black=0.006).to_ImageClip(tfreeze)
+im_freeze = test_video.to_ImageClip(tfreeze)
+painting = test_video.fx(vfx.painting, saturation=1.6, black=0.006).to_ImageClip(tfreeze)
 
-txt = TextClip("Audrey", font="Amiri-regular", font_size=35)
+txt = TextClip("Audrey", font="Amiri-regular", font_size=35, )
 
 painting_txt = (
-    CompositeVideoClip([painting, txt.set_pos((10, 180))])
+    CompositeVideoClip([painting])
     .add_mask()
     .with_duration(3)
     .crossfadein(0.5)
@@ -33,10 +31,7 @@ painting_fading = CompositeVideoClip([im_freeze, painting_txt])
 
 # FINAL CLIP AND RENDERING
 
-final_clip = concatenate_videoclips(
-    [clip_before, painting_fading.with_duration(3), clip_after]
-)
+final_clip = concatenate_videoclips([clip_before, painting_fading.with_duration(3), clip_after])
 
-final_clip.write_videofile(
-    "../../audrey.avi", fps=charade.fps, codec="mpeg4", audio_bitrate="3000k"
-)
+final_clip.write_videofile("media/audrey_edited.mp4", fps=test_video.fps, temp_audiofile="temp-audio.m4a",
+                           remove_temp=True, codec="libx264", audio_codec="aac")
