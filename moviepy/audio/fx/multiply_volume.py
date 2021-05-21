5c4c5a220845d6e3c6daa943b2fa239dee0fb07e
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-05-21 10:10:13 +0200

    Add 'start' and 'end' parameters to multiply_volume FX

--- moviepy/audio/fx/multiply_volume.py
+++ moviepy/audio/fx/multiply_volume.py
@@ -1,22 +1,75 @@
+import numpy as np
+
 from moviepy.decorators import audio_video_fx
 
 
+def _multiply_volume_in_range(factor, start, end, nchannels):
+    def factors_filter(factor, t):
+        return np.array([factor if start <= t_ <= end else 1 for t_ in t])
+
+    def multiply_stereo_volume(get_frame, t):
+        return np.multiply(
+            get_frame(t),
+            np.array([factors_filter(factor, t) for _ in range(nchannels)]).T,
+        )
+
+    def multiply_mono_volume(get_frame, t):
+        return np.multiply(get_frame(t), factors_filter(factor, t))
+
+    return multiply_mono_volume if nchannels == 1 else multiply_stereo_volume
+
+
 @audio_video_fx
-def multiply_volume(clip, factor):
+def multiply_volume(clip, factor, start=None, end=None):
     """Returns a clip with audio volume multiplied by the
     value `factor`. Can be applied to both audio and video clips.
 
-    This effect is loaded as a clip method when you use moviepy.editor,
-    so you can just write ``clip.multiply_volume(2)``
+    Parameters
+    ----------
+
+    factor : float
+      Volume multiplication factor.
+
+    start : float, optional
+      Time from the beginning of the clip until the volume transformation
+      begins to take effect, in seconds. By default at the beginning.
+
+    end : float, optional
+      Time from the beginning of the clip until the volume transformation
+      ends to take effect, in seconds. By default at the end.
 
     Examples
     --------
 
     >>> from moviepy import AudioFileClip
+    >>>
     >>> music = AudioFileClip('music.ogg')
-    >>> new_clip = clip.multiply_volume(2)  # doubles audio volume
-    >>> new_clip = clip.multiply_volume(0.5)  # half audio
+    >>> doubled_audio_clip = clip.multiply_volume(2)  # doubles audio volume
+    >>> half_audio_clip = clip.multiply_volume(0.5)  # half audio
+    >>>
+    >>> # silenced clip during one second at third
+    >>> silenced_range_audio_clip = clip.multiply_volume(0, start=2, end=3)
     """
+    if start is None and end is None:
+        return clip.transform(
+            lambda get_frame, t: factor * get_frame(t),
+            keep_duration=True,
+        )
+
+    start = clip.start if start is None else start
+    end = clip.duration if end is None else end
+
+    if hasattr(clip, "nchannels"):
+        nchannels = clip.nchannels
+    else:
+        first_frame = clip.get_frame(start)
+        nchannels = (
+            first_frame.shape[1]
+            if (hasattr(first_frame, "shape") and len(first_frame.shape) > 1)
+            else 1
+        )
+
     return clip.transform(
-        lambda get_frame, t: factor * get_frame(t), keep_duration=True
+        _multiply_volume_in_range(factor, start, end, nchannels),
+        keep_duration=True,
     )
