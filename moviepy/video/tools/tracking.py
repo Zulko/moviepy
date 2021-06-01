0f3d9ec9c2fe598639967d1edee72eb14a3a3446
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-06-01 21:22:48 +0200

    Improve docstring

--- moviepy/video/tools/tracking.py
+++ moviepy/video/tools/tracking.py
@@ -179,9 +179,10 @@ def findAround(pic, pat, xy=None, r=None):
 def autoTrack(clip, pattern, tt=None, fps=None, radius=20, xy0=None):
     """Tracks a given pattern (small image array) in a video clip.
 
-    Returns ``[(x1, y1), (x2, y2)...]`` where xi,yi are the coordinates of the
-    pattern in the clip on frame ``i``. To select the frames you can either
-    specify a list of times with ``tt`` or select a frame rate with ``fps``.
+    Returns ``[(x1, y1), (x2, y2)...]`` where ``(xi, yi)`` are the coordinates
+    of the pattern in the clip on frame ``i``. To select the frames you can
+    either specify a list of times with ``tt`` or select a frame rate with
+    ``fps``.
 
     This algorithm assumes that the pattern's aspect does not vary much and
     that the distance between two occurences of the pattern in two consecutive
