acb87ce4dbb5e4c4c08991b9a26f266058c3e60a
hossien014 <hossien014@gmail.com>
2023-12-05 11:47:31 +0330

    Stop tracking .venv folder

--- .venv/include/site/python3.10/pygame/pgbufferproxy.h
+++
@@ -1,7 +0,0 @@
-#ifndef PG_BUFPROXY_INTERNAL_H
-#define PG_BUFPROXY_INTERNAL_H
-
-#include "include/pygame_bufferproxy.h"
-#define PYGAMEAPI_BUFPROXY_NUMSLOTS 4
-
-#endif /* ~PG_BUFPROXY_INTERNAL_H */
