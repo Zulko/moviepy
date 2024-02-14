dbc0d352a2c41bc9be24e163b962531ec3cab9ed
vermont <uqzx@protonmail.com>
2024-02-14 06:16:41 -0500

    Support old "lastread" name as property, for existing libraries that use it.

--- moviepy/video/io/ffmpeg_reader.py
+++ moviepy/video/io/ffmpeg_reader.py
@@ -249,6 +249,13 @@ class FFMPEG_VideoReader:
         # are getting the nth frame by writing get_frame(n/fps).
         return int(self.fps * t + 0.00001)
 
+    @property
+    def lastread(self):
+        """Support old name of the "last_read" attribute, which may be used
+        by existing libraries, such as scenedetect.
+        """
+        return self.last_read
+
     def close(self, delete_last_read=True):
         """Closes the reader terminating the process, if is still open."""
         if self.proc:
